9010c8dc2fa021d1b418725877605074012e4dd0
silvio-progonati <silvioprogonati@gmail.com>
2024-07-13 10:29:03 +0200

    Add Parametarized Tests

--- src/test/java/executor/AdditionExecutorTest.java
+++ src/test/java/executor/AdditionExecutorTest.java
@@ -1,8 +1,10 @@
 package executor;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.stream.Stream;
 
@@ -10,19 +12,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AdditionExecutorTest {
     
+    
     @ParameterizedTest
-    @MethodSource("getAdditionArguments")
-    void execute(int firstArgument, int secondArgument, int expected) {
-        int actual = new AdditionExecutor(firstArgument, secondArgument).execute();
-        assertEquals(expected, actual);
+    @MethodSource("provideArguments")
+    void execute(int firstArgument, int secondArgument, int result) {
+        //Arrange
+        AdditionExecutor additionExecutor = new AdditionExecutor(firstArgument, secondArgument);
+        
+        //Act & Assert
+        assertEquals(result, additionExecutor.execute());
     }
     
-    private static Stream<Arguments> getAdditionArguments() {
+    private static Stream<Arguments> provideArguments() {
         return Stream.of(
                 Arguments.of(1, 2, 3),
-                Arguments.of(14, 456, 470),
-                Arguments.of(0, 5, 5),
-                Arguments.of(-3, 6, 3)
+                Arguments.of(2, -1, 1),
+                Arguments.of(0, 3, 3),
+                Arguments.of(3, 0, 3)
         );
     }
+    
+    
 }
